5451ddb71d236f74d0cb67aef9b1853e16a903c0
Joseph <thele116@morris.umn.edu>
2015-04-06 20:14:27 -0500

    Did a bunch of stuff. It mostly works now, but needs some improvment.

--- MoveRobot/MoveRobot.ino
+++ MoveRobot/MoveRobot.ino
@@ -28,10 +28,20 @@
 #include <Wire.h>
 #include <NXShield.h>
 #include <NXTUS.h>
-#include <NXTMMX.h>
+#include <NXTMMX.h>s
 
 #include <SoftwareSerial.h>
 //Don't make # symbols
+
+//
+// Config Values
+//
+const int HOOP_DIST = 60;
+const int UPDATE_DELAY = 50;
+const int SCAN_SPEED = 50;
+const int SCAN_SIDE_DIST = 7;
+const int BACK_DIST = 40;
+const int BACK_THRESH = 3;
 
 // The shield
 NXShield nxshield;
@@ -43,18 +53,10 @@ NXTUS       sonarLeft; // left
 NXTUS       sonarRight; // right
 NXTUS       sonarBack;
 NXTMMX      mmx(0x0A); // Multiplexer for chain morors
-//NXTTouch    touchSensor; // used for chain endstops
+NXTTouch    endStop; // used for chain endstops
 
 // 2 line LCD on arduino digital pin #6
 SoftwareSerial lcd(2, 5);
-
-//
-// Config Values and loop tuneing.
-//
-int updateDelay = 50; // X ms sensor / screen update time
-int nudgeTime = 180; // oifnviownerfvwwefvpinm
-int scanSpeed = 50;
-int scanSideDist = 7; 
 
 
 ///
@@ -77,6 +79,8 @@ void setup(){
   if(batVolt < 7.50) {
     clearDisplay();
     beep();
+    Serial.println("Low Voltage!");
+    Serial.println(batVolt);
     lcd.print("LOW VOLTAGE!!!");
     setLCDCursor(16);
     lcd.print("voltage: ");
@@ -100,50 +104,53 @@ void setup(){
   sonarLeft.init( &nxshield, SH_BAS2 );
   sonarRight.init( &nxshield, SH_BBS1 );
   sonarBack.init( &nxshield, SH_BBS2 );  
-//  mmx.init( &nxshield, SH_BAS1);
+  endStop.init( &nxshield, SH_BAS1 );
   
   // sensor diagnostic mode.
-if(nxshield.getButtonState(BTN_RIGHT)){
-   clearDisplay();
-   lcd.print("Diagnostic Mode!");
-   delay(5000);
-   int backUS = sonarBack.getDist();
-   int rightUS = sonarRight.getDist();
-   int leftUS = sonarLeft.getDist();
-   //boolean endStops = touchSensor.isPressed();
-   while(true){
-     delay(updateDelay);
-     backUS = sonarBack.getDist();
-     rightUS = sonarLeft.getDist();
-     leftUS = sonarRight.getDist();
-     
+  if(nxshield.getButtonState(BTN_RIGHT)){
      clearDisplay();
-     lcd.print("bUS: ");
-     lcd.print(backUS);
-     lcd.print(" rUS: ");
-     lcd.print(rightUS);
-     setLCDCursor(16);
-     lcd.print("lUS: ");
-     lcd.print(leftUS);
-     lcd.print(" btn: ");
-     lcd.print("NA");
+     beep();
+     lcd.print("Diagnostic Mode!");
+     delay(5000);
+     int backUS = sonarBack.getDist();
+     int rightUS = sonarRight.getDist();
+     int leftUS = sonarLeft.getDist();
+     //boolean endStops = touchSensor.isPressed();
+     while(true){
+       delay(UPDATE_DELAY);
+       backUS = sonarBack.getDist();
+       rightUS = sonarLeft.getDist();
+       leftUS = sonarRight.getDist();
      
-     if(nxshield.getButtonState(BTN_RIGHT)){
-       elevatorRaise(100);
-     }else if(nxshield.getButtonState(BTN_LEFT)){
-       elevatorLower(100);
-     }else {
-       elevatorPassiveStop();
-     } 
-   }
+       clearDisplay();
+       lcd.print("bUS: ");
+       lcd.print(backUS);
+       lcd.print(" rUS: ");
+       lcd.print(rightUS);
+       setLCDCursor(16);
+       lcd.print("lUS: ");
+       lcd.print(leftUS);
+       lcd.print(" btn: ");
+       lcd.print("NA");
+     
+       if(nxshield.getButtonState(BTN_RIGHT)){
+         elevatorRaise(100);
+       }else if(nxshield.getButtonState(BTN_LEFT)){
+         elevatorLower(100);
+       }else {
+         elevatorPassiveStop();
+       } 
+     }
     
-}
+  } // end diagnostics mode
+  
+  elevatorRaise_Degrees(40, 180);
     
  //   approachHoop();
  //  findCenter(2);
  //   takeAShot();
-
-  
+ 
+  clearDisplay();
 } // end setup
 
 ///
@@ -151,21 +158,242 @@ if(nxshield.getButtonState(BTN_RIGHT)){
 ///
 
 void loop(){
-  clearDisplay();
-  lcd.print("Looping");
-  delay(1000);
-  lcd.print(".");
-  delay(1000);
-  lcd.print(".");
-  delay(1000);
-  lcd.print(".");
-  delay(1000);
+  scan();
 }
 
 
 ///
 /////////// Helper Functions /////////////////////////////////////////////// 
 ///
+
+// dist - desired distance (~cm) from the back wall of the field.
+// backThresh - the threshold (~cm) before the robot will attempt to correct for drift.
+// ballThresh - the threshold (~cm) of sudden distance change that will trigger a ball found state.
+// sideDist - the distance (~cm) from the sides of the field that the robot will maintain while scanning.
+
+void scan() {
+  int backDist = sonarBack.getDist();
+  int distSum = 0;
+  int sensorCount = 0;
+  int scanCount = 0;
+  boolean  foundBall = false;
+  //elevatorReadyPosition();
+  while(!foundBall){
+    alignLeft(SCAN_SIDE_DIST, BACK_THRESH);
+    distSum = 0;
+    strafeRight(SCAN_SPEED);
+   
+    while(sonarLeft.getDist() > SCAN_SIDE_DIST){
+      delay(UPDATE_DELAY);
+      clearDisplay();
+            
+      backDist = sonarBack.getDist();
+      distSum += backDist;
+      
+      lcd.print("scanning...");
+      setLCDCursor(16);
+      lcd.print(backDist);
+      
+      if(backDist > 105 || backDist < 10) {
+        foundBall = true;
+        stopMoving();
+        grabBall(50);
+        approachHoop();
+       takeAShot();
+        
+        break;
+      }
+    } //end strafe while
+    
+    if(!foundBall) {
+      delay(900);
+      stopMoving();
+      approachBackWall(BACK_DIST, BACK_THRESH);
+    }
+  } //end find ball while
+  //void alignLeft(int backDist, int sideDist, int backThresh);
+}
+
+
+
+// Moves forward and grabs a ball (assumes ball is there...)
+void grabBall(int speed) {
+ 
+ clearDisplay();
+ lcd.print("grabBall()...");
+ 
+ stopMoving();
+ elevatorToFloor();
+ delay(100);
+ moveBackward(speed);
+ delay(2300);
+ stopMoving();
+ delay(100);
+ moveForward(20);
+ delay(500);
+ elevatorRaise_Degrees(50, 180);
+ delay(200);
+ moveBackward(30);
+ delay(1500);
+ moveForward(50);
+ delay(1500);
+ stopMoving();
+
+ clearDisplay(); 
+ }
+
+// Takes a shot./////////////////////////////////////////////////////////////////////////////////////
+void takeAShot(){
+  
+  clearDisplay();
+  lcd.print("Taking the shot!");
+  
+  beep();
+  stopMoving();
+  elevatorRaise(100);
+  while(!endStop.isPressed()){};
+  elevatorReadyPosition();
+  
+  clearDisplay();
+}
+
+// moves the bot to within dist cm, give or take thresh cm, of the back wall
+void approachBackWall(int dist, int thresh) {
+  
+  clearDisplay();
+  lcd.print("approching back wall...");
+ 
+  int curDist = sonarBack.getDist() - dist;
+  
+  while(abs(curDist) > thresh)  {
+    delay(UPDATE_DELAY);
+    curDist = sonarBack.getDist() - dist;
+    
+    clearDisplay();
+    lcd.print("approachBackWall()"); 
+    setLCDCursor(16);
+    lcd.print("curDist: ");
+    lcd.print(curDist);
+    
+    if (curDist < 0) {
+      moveForward(50);
+    } else {
+      moveBackward(50);
+    }
+  }
+  stopMoving();
+  
+  clearDisplay();
+}
+
+
+// centers the robot, and then moves forward till the bot is ~62cm from the hoop wall.
+void approachHoop() {
+  findCenter(2);
+  clearDisplay();
+  lcd.write("approachHoop()");
+  
+  delay(500);
+  
+  moveForward(50);
+  while(sonarBack.getDist() < HOOP_DIST) { 
+  }
+  
+  clearDisplay();
+  stopMoving();
+}
+
+// a really silly method that just moves the thing left. hopefully. 
+void alignLeft(int sideDist, int backThresh){
+ clearDisplay();
+ lcd.write("aligning left...");
+ 
+ int distB = sonarRight.getDist() - sideDist;
+ int curDist = sonarBack.getDist() - BACK_DIST;
+ int ballDist = sonarBack.getDist();
+ 
+ while(distB > 0) {
+  delay(UPDATE_DELAY); 
+  distB = sonarRight.getDist() - sideDist; 
+  curDist = abs(sonarBack.getDist() - BACK_DIST) ;
+  ballDist = sonarBack.getDist();
+
+  strafeLeft(SCAN_SPEED); 
+  
+  clearDisplay();
+  lcd.print("Aligning left...");
+  setLCDCursor(16);
+  lcd.print("left dist: ");
+  lcd.print(distB);
+  
+    if(ballDist > 100 || ballDist < 10) {
+        stopMoving();
+        grabBall(40);
+        approachHoop();
+        takeAShot();
+        break;
+    }
+    /*
+  if(curDist > backThresh) {
+    //stopMoving();
+    clearDisplay();
+    lcd.write("correcting dist. !");
+    delay(100);
+    approachBackWall(BACK_DIST, 3);
+  }
+  */
+
+ } // end while
+ delay(1000);
+ stopMoving();
+ approachBackWall(BACK_DIST, 3);
+ clearDisplay();
+}
+
+// centers the robot left / right on the field
+void findCenter(int threshold) {
+  int distA = sonarLeft.getDist();
+  int distB = sonarRight.getDist();
+  
+  clearDisplay();
+  lcd.write("findCenter()...");
+  
+  while(abs(distA - distB) > threshold) {
+  delay(UPDATE_DELAY);
+    distA = sonarLeft.getDist();
+    distB = sonarRight.getDist();
+    
+    clearDisplay();
+    lcd.print("Finding Center...");
+    setLCDCursor(16);
+    lcd.print("A: ");
+    lcd.print(distA);
+    lcd.print(" B: ");
+    lcd.print(distB);
+
+    if(distA > distB) {
+      strafeRight(40); 
+    } else {
+      strafeLeft(40);
+    }
+  }
+  
+  clearDisplay();
+  stopMoving();
+}
+
+
+void breakTime(){
+  findCenter(4);
+  approachHoop();
+  stopMoving();
+  clearDisplay();
+  lcd.print("taking a break...");
+  delay(2000);
+  clearDisplay();
+}
+
+//////////////////////////// MISC //////////////////////////////////////
 
 // Activates the noise maker
 void beep() {
@@ -184,263 +412,6 @@ void coloryLights(){
   nxshield.ledSetRGB(0,0,0);
   delay(1000);
 }
-
-// dist - desired distance (~cm) from the back wall of the field.
-// backThresh - the threshold (~cm) before the robot will attempt to correct for drift.
-// ballThresh - the threshold (~cm) of sudden distance change that will trigger a ball found state.
-// sideDist - the distance (~cm) from the sides of the field that the robot will maintain while scanning.
-
-/**
-void scan(int dist, int backThresh) {
-  findCenter(3);
-  approachBackWall(dist, backThresh);
-  
-  int backDist = sonarBack.getDist();
-  int distSum = 0;
-  int averageDist = 0;
-  int sensorCount = 0;
-  int scanCount = 0;
-  boolean  foundBall = false;
-  
-  while(!foundBall){
-    scanCount++; 
-    
-    alignLeft(dist, scanSideDist, backThresh);
-    distSum = 0;
-    averageDist = 0;
-    sensorCount = 0;
-    strafeRight(scanSpeed);
-   
-    while(sonarLeft.getDist() > scanSideDist){
-      delay(updateDelay);
-      
-      sensorCount++;      
-      if(sensorCount > 30){
-         sensorCount = 0;
-         distSum = 0; 
-         averageDist = 0;
-      }
-      
-      backDist = sonarBack.getDist();
-      distSum += backDist;
-      averageDist = distSum / sensorCount; 
-      
-      clearDisplay();
-      lcd.print(backDist);
-      setLCDCursor(16);
-      lcd.print(averageDist);
-      
-      if(backDist > 100) {
-        foundBall = true;
-        stopMoving();
-        grabBall(40);
-        //approachHoop();
-        takeAShot();
-        
-        break;
-      }
-    } //end strafe while
-    
-    if(!foundBall) {
-      delay(1000);
-      stopMoving();
-      approachBackWall(dist, backThresh);
-    }
-  } //end find ball while
-  
-  //void alignLeft(int backDist, int sideDist, int backThresh)
-}
-*/
-
-
-// Moves forward and grabs a ball (assumes ball is there...)
-
-/**
-void grabBall(int speed) {
- stopMoving();
- delay(100);
- moveBackward(speed);
- delay(2500);
- stopMoving();
- delay(100);
- moveForward(20);
- delay(100);
- moveBackward(30);
- delay(1500);
- moveForward(50);
- delay(1500);
- stopMoving(); 
- //approachHoop();
- }
-**/
- 
-
-// Takes a shot.
-void takeAShot(){
-  beep();
-  stopMoving();
-  clearDisplay();
-}
-
-/**
-// moves the bot to within dist cm, give of take thresh cm, of the back wall
-void approachBackWall(int dist, int thresh) {
-  clearDisplay();
-  lcd.print("approching back wall!");
-  int curDist = sonarBack.getDist() - dist;
-  
-  while(abs(curDist) > thresh)  {
-    delay(updateDelay);
-    curDist = sonarBack.getDist() - dist;
-    
-    clearDisplay();
-    lcd.print("approachBackWall()"); 
-    setLCDCursor(16);
-    lcd.print("curDist: ");
-    lcd.print(curDist);
-    
-    if (curDist < 0) {
-      moveForward(50);
-    } else {
-      moveBackward(50);
-    }
-  }
-  stopMoving();
-  clearDisplay();
-}
-*/
-
-/*
-// centers the robot, and then moves forward till the bot is ~62cm from the hoop wall.
-void approachHoop() {
-  //findCenter(3);
-  clearDisplay();
-  lcd.write("approachHoop()");
-  
-  if(abs(sonarLeft.getDist() - sonarRight.getDist()) > 6){
-    findCenter(3);
-  }
-  
-  delay(500);
-  
-  moveForward(50);
-  while(sonarBack.getDist() < 60) { 
-  }
-  
-  clearDisplay();
-  stopMoving();
-}
-**/
-
-/**
-// a really silly method that just moves the thing left. hopefully. 
-void alignLeft(int backDist, int sideDist, int backThresh){
- clearDisplay();
- lcd.write("aligning left...");
- 
- int distB = sonarRight.getDist() - sideDist;
- int curDist = sonarBack.getDist() - backDist;
- int ballDist = sonarBack.getDist();
- 
- while(distB > 0) {
-  delay(updateDelay); 
-  distB = sonarRight.getDist() - sideDist; 
-  curDist = abs(sonarBack.getDist() - backDist) ;
-  ballDist = sonarBack.getDist();
-
-
-  strafeLeft(scanSpeed); 
-  
-  clearDisplay();
-  lcd.print("Aligning left...");
-  setLCDCursor(16);
-  lcd.print("left dist: ");
-  lcd.print(distB);
-  
-    if(ballDist > 100) {
-        stopMoving();
-        grabBall(40);
-        //approachHoop();
-        takeAShot();
-        break;
-    }
-//  if(curDist > backThresh) {
-//    //stopMoving();
-//    clearDisplay();
-//    lcd.write("correcting dist. !");
-//    delay(100);
-//    approachBackWall(backDist, 3);
-//  }
-**/
-
-/**
- }
- delay(1000);
- stopMoving();
-     if(cornerScan){
-       frontWheelsGo();
-       moveBackward(30);
-       delay(2000);
-       stopMoving();
-       strafeRight(30);
-       delay(2000);
-       stopMoving();
-       moveForward(30);
-       delay(2000);
-       stopMoving(); 
-       frontWheelsStop();
-    }
-   approachBackWall(backDist, 3);
-   clearDisplay();
-}
-**/
-
-/**
-// centers the robot left / right on the field
-void findCenter(int threshold) {
-  int distA = sonarLeft.getDist();
-  int distB = sonarRight.getDist();
-  
-  clearDisplay();
-  lcd.write("findCenter()..");
-  
-  while(abs(distA - distB) > threshold) {
-  delay(updateDelay);
-    distA = sonarLeft.getDist();
-    distB = sonarRight.getDist();
-    
-    clearDisplay();
-    lcd.print("Finding Center...");
-    setLCDCursor(16);
-    lcd.print("A: ");
-    lcd.print(distA);
-    lcd.print(" B: ");
-    lcd.print(distB);
-
-    if(distA > distB) {
-      strafeRight(50); 
-    } else {
-      strafeLeft(50);
-    }
-  }
-  
-  clearDisplay();
-  stopMoving();
-}
-**/
-
-/**
-void breakTime(){
-  approachHoop();
-  findCenter(3);
-  stopMoving();
-  clearDisplay();
-  lcd.print("taking a break...");
-  delay(2000);
-  clearDisplay();
-}
-**/
-
 
 /////////////////////////// MOVEMENT COMMANDS //////////////////////////
 
@@ -512,22 +483,22 @@ diagonal(){
 
 ///////////////////////////// ELEVATOR COMMANDS ////////////////////////////
 
-void elevatorLower_Rotations(int speed1, long rotations) {
-  mmx.runRotations(MMX_Motor_1, MMX_Direction_Forward, speed1, rotations, MMX_Completion_Dont_Wait, MMX_Next_Action_Float);
-  mmx.runRotations(MMX_Motor_2, MMX_Direction_Reverse, speed1, rotations, MMX_Completion_Dont_Wait, MMX_Next_Action_Float); 
+void elevatorRaise_Degrees(int speed1, long deg) {
+  mmx.runDegrees(MMX_Motor_1, MMX_Direction_Forward, speed1, deg, MMX_Completion_Dont_Wait, MMX_Next_Action_Float);
+  mmx.runDegrees(MMX_Motor_2, MMX_Direction_Reverse, speed1, deg, MMX_Completion_Dont_Wait, MMX_Next_Action_Float); 
 }
 
-void elevatorRaise_Rotations(int speed1, long rotations) {
-  mmx.runRotations(MMX_Motor_1, MMX_Direction_Reverse, speed1, rotations, MMX_Completion_Dont_Wait, MMX_Next_Action_Float);
-  mmx.runRotations(MMX_Motor_2, MMX_Direction_Forward, speed1, rotations, MMX_Completion_Dont_Wait, MMX_Next_Action_Float); 
+void elevatorLower_Degrees(int speed1, long deg) {
+  mmx.runDegrees(MMX_Motor_1, MMX_Direction_Reverse, speed1, deg, MMX_Completion_Dont_Wait, MMX_Next_Action_Float);
+  mmx.runDegrees(MMX_Motor_2, MMX_Direction_Forward, speed1, deg, MMX_Completion_Dont_Wait, MMX_Next_Action_Float); 
 }
 
-void elevatorLower(int speed1){
+void elevatorRaise(int speed1){
   mmx.runUnlimited(MMX_Motor_1, MMX_Direction_Forward, speed1); 
   mmx.runUnlimited(MMX_Motor_2, MMX_Direction_Reverse, speed1);
 }
 
-void elevatorRaise(int speed1){
+void elevatorLower(int speed1){
   mmx.runUnlimited(MMX_Motor_1, MMX_Direction_Reverse, speed1); 
   mmx.runUnlimited(MMX_Motor_2, MMX_Direction_Forward, speed1);
 }
@@ -535,6 +506,29 @@ void elevatorRaise(int speed1){
 void elevatorPassiveStop() {
   mmx.runSeconds(MMX_Motor_Both, MMX_Direction_Reverse, 0, 0, MMX_Completion_Dont_Wait, MMX_Next_Action_Brake);
   mmx.runSeconds(MMX_Motor_Both, MMX_Direction_Reverse, 0, 0, MMX_Completion_Dont_Wait, MMX_Next_Action_Brake);
+}
+
+void elevatorReadyPosition() {
+ elevatorLower(50);
+ delay(300);
+ while(mmx.getMotorStatusByte(MMX_Motor_1) < 16){
+    delay(UPDATE_DELAY);
+    clearDisplay();
+    lcd.print(mmx.getMotorStatusByte(MMX_Motor_1));
+ };
+ elevatorPassiveStop();
+ elevatorRaise_Degrees(50, 180);
+}
+
+void elevatorToFloor() {
+ elevatorLower(40);
+ delay(100);
+ while(mmx.getMotorStatusByte(MMX_Motor_1) < 16){
+   delay(UPDATE_DELAY);
+   clearDisplay();
+   lcd.print(mmx.getMotorStatusByte(MMX_Motor_1));
+ }
+ elevatorPassiveStop();
 }
 
 ///////////////////////////// LCD COMMANDS //////////////////////////////
@@ -548,4 +542,3 @@ void setLCDCursor(byte cursor_position){
  lcd.write(0x80); // ready LCD to recieve cursor potition
  lcd.write(cursor_position); // send cursor position 
 }
-#
